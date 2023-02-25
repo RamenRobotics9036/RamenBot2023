0ede92d42179a0a220af5db556ac037f6c8010df
RamenRobotics9036 <ramenrobotics0@gmail.com>
2023-02-24 16:06:22 -0800

    Add starting positions to autonomous.

--- src/main/java/frc/robot/Commands/Auto.java
+++ src/main/java/frc/robot/Commands/Auto.java
@@ -14,22 +14,21 @@ public class Auto {
     }
 
     public static CommandBase getAutoCommand(TankDriveSystem m_driveSystem, ArmSystem m_armSystem, GrabberSystem m_grabSystem) {
-        return getCommandSequence(m_driveSystem, m_armSystem, m_grabSystem);
-    }
-
-    private static CommandBase getCommandSequence(TankDriveSystem m_driveSystem, ArmSystem m_armSystem, GrabberSystem m_grabSystem) {
-        if (SmartDashboard.getNumber("Position", 0) == 0) {
+        if (SmartDashboard.getNumber("Starting Position", 0) == 0) {
             return Commands.sequence(
                 new DriveForwardCommand(m_driveSystem, 1, Constants.OperatorConstants.kGearBoxRatioDrive, 0.5)
             );
-        } else if (SmartDashboard.getNumber("Position", 0) == 1) {
-            return Commands.sequence(null);
-        } else if (SmartDashboard.getNumber("Position", 0) == 2) {
-            return Commands.sequence(null);
+        } else if (SmartDashboard.getNumber("Starting Position", 0) == 1) {
+            return Commands.sequence(
+                new DriveForwardCommand(m_driveSystem, 1, Constants.OperatorConstants.kGearBoxRatioDrive, 0.5)
+            );
+        } else if (SmartDashboard.getNumber("Starting Position", 0) == 2) {
+            return Commands.sequence(
+                new DriveForwardCommand(m_driveSystem, 1, Constants.OperatorConstants.kGearBoxRatioDrive, 0.5)
+            );
         }
-        return null; // 0 is left, 1 is middle, 2 is right
+        return null; // 0 is left, 1 is middle, 2 is right    
     }
-
     public static void initShuffleBoardCommands(TankDriveSystem m_driveSystem, ArmSystem m_armSystem, GrabberSystem m_grabSystem) {
         SmartDashboard.putNumber("Auto Motor Speed", Constants.OperatorConstants.kAutoMotorSpeed);
         SmartDashboard.putNumber("Auto Motor Rotations", Constants.OperatorConstants.kAutoMotorRotations);
