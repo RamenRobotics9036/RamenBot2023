ee73a501e560bd2edee806f6f684f102345f3de8
Ido <idoben2@hotmail.com>
2023-03-31 17:22:41 -0700

    Undo post-season checkins that were accidentally done

--- src/main/java/frc/robot/Commands/SetWinchToAngle.java
+++ src/main/java/frc/robot/Commands/SetWinchToAngle.java
@@ -3,15 +3,15 @@ package frc.robot.Commands;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.Subsystems.WinchSystem;
+import frc.robot.Subsystems.ArmSystem;
 
 public class SetWinchToAngle extends CommandBase {
-    private WinchSystem m_armSystem;
+    private ArmSystem m_armSystem;
     private double angle;
     private double speed;
     private int inverse;
 
-    public SetWinchToAngle(WinchSystem m_armSystem, double angle, double speed) {
+    public SetWinchToAngle(ArmSystem m_armSystem, double angle, double speed) {
         this.m_armSystem = m_armSystem;
         addRequirements(m_armSystem);
 
