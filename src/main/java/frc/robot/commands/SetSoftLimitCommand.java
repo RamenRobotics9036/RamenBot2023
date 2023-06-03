a27377ba97048f2b952894123e0aacc4f12a41d2
Ido <idoben2@hotmail.com>
2023-06-02 17:42:55 -0700

    Cleanup
    Cleanup

--- src/main/java/frc/robot/commands/SetSoftLimitCommand.java
+++ src/main/java/frc/robot/commands/SetSoftLimitCommand.java
@@ -3,17 +3,20 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Subsystems.ArmSystem;
 
+/**
+ * Command to set the soft limit of the arm.
+ */
 public class SetSoftLimitCommand extends CommandBase {
-  ArmSystem armSystem;
+  ArmSystem m_armSystem;
 
   public SetSoftLimitCommand(ArmSystem armSystem) {
-    this.armSystem = armSystem;
+    this.m_armSystem = armSystem;
     addRequirements(armSystem);
   }
 
   @Override
   public void initialize() {
-    armSystem.setSoftLimit();
+    m_armSystem.setSoftLimit();
   }
 
   @Override
@@ -27,6 +30,6 @@ public class SetSoftLimitCommand extends CommandBase {
 
   @Override
   public void end(boolean interrupted) {
-    armSystem.setExtenderSpeed(0);
+    m_armSystem.setExtenderSpeed(0);
   }
 }
--- src/main/java/frc/robot/commands/SetWinchToAngle.java
+++ src/main/java/frc/robot/commands/SetWinchToAngle.java
@@ -5,38 +5,44 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.Subsystems.ArmSystem;
 
+/**
+ * Command to set the winch to a certain angle.
+ */
 public class SetWinchToAngle extends CommandBase {
   private ArmSystem m_armSystem;
-  private double angle;
-  private double speed;
-  private int inverse;
+  private double m_angle;
+  private double m_speed;
+  private int m_inverse;
 
-  public SetWinchToAngle(ArmSystem m_armSystem, double angle, double speed) {
-    this.m_armSystem = m_armSystem;
-    addRequirements(m_armSystem);
+  /**
+   * Constructor.
+   */
+  public SetWinchToAngle(ArmSystem armSystem, double angle, double speed) {
+    this.m_armSystem = armSystem;
+    addRequirements(armSystem);
 
-    this.angle = angle;
-    this.speed = speed;
+    this.m_angle = angle;
+    this.m_speed = speed;
   }
 
   @Override
   public void initialize() {
     double currentRotations = m_armSystem.getWinchAbsoluteEncoder();
-    if (currentRotations > angle) {
-      inverse = -1;
+    if (currentRotations > m_angle) {
+      m_inverse = -1;
       System.out.println("inversed");
     }
     else {
-      inverse = 1;
+      m_inverse = 1;
       System.out.println("not inversed");
     }
     System.out.println("Command initialized with enoder at " + currentRotations);
-    System.out.println("Command initialized with speed at " + speed);
+    System.out.println("Command initialized with speed at " + m_speed);
   }
 
   @Override
   public void execute() {
-    m_armSystem.setWinchSpeed(inverse * speed);
+    m_armSystem.setWinchSpeed(m_inverse * m_speed);
     System.out.println("Command executed " + m_armSystem.getWinchAbsoluteEncoder());
   }
 
@@ -48,12 +54,12 @@ public class SetWinchToAngle extends CommandBase {
 
     }
     System.out.println("ENCODER" + m_armSystem.getWinchAbsoluteEncoder());
-    System.out.println("ANGLE" + angle);
-    if (inverse == 1 && m_armSystem.getWinchAbsoluteEncoder() >= angle) {
+    System.out.println("ANGLE" + m_angle);
+    if (m_inverse == 1 && m_armSystem.getWinchAbsoluteEncoder() >= m_angle) {
       System.out.println("Is finished with encoder at " + m_armSystem.getWinchAbsoluteEncoder());
       return true;
     }
-    else if (inverse == -1 && m_armSystem.getWinchAbsoluteEncoder() <= angle) {
+    else if (m_inverse == -1 && m_armSystem.getWinchAbsoluteEncoder() <= m_angle) {
       System.out.println("Is finished with encoder at " + m_armSystem.getWinchAbsoluteEncoder());
       return true;
     }
